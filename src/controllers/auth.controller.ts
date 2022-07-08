082daf49e076bce37b13bbd3f008401dac64ffa2
Lucas Pereyra <lucasezequielpereyra0@gmail.com>
2022-07-07 23:13:23 -0300

    login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,3 @@
-import { IRole } from './../interfaces/role.interface';
 import { Request, Response } from 'express';
 import { AuthService } from '../services/auth.service';
 import { RoleService } from '../services/role.service';
@@ -7,6 +6,12 @@ import logger from '../config/logger';
 
 const authService = new AuthService();
 const roleService = new RoleService();
+
+declare module 'express-session' {
+  interface SessionData {
+    passport: any;
+  }
+}
 
 export class AuthController {
   async signup(req: Request, res: Response) {
@@ -40,7 +45,12 @@ export class AuthController {
   }
 
   async successredirect(req: Request, res: Response) {
-    res.status(200).json('Success');
+    if (req.isAuthenticated()) {
+      const token: string = authService.getUserToken(req.session.passport.user);
+      req.session.passport.token = token;
+    } else {
+      res.status(401).json('unauthorized');
+    }
   }
 
   async failureredirect(req: Request, res: Response) {
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { IUser } from '../interfaces/user.interface';
 import { UserModel } from '../models/user.model';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export class AuthService {
   async newUser(user: IUser): Promise<IUser> {
@@ -21,5 +22,11 @@ export class AuthService {
     userPassword: string,
   ): Promise<boolean> {
     return await bcrypt.compare(password, userPassword);
+  }
+
+  getUserToken(user: IUser): string {
+    return jwt.sign({ id: user._id }, String(process.env.JWT_PHRASE), {
+      expiresIn: 1000 * 60 * 60 * 24,
+    });
   }
 }
