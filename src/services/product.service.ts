9c26ea8a1f3e2dd8d896490a296fb3a67872d280
Lucas Pereyra <lucasezequielpereyra0@gmail.com>
2022-07-12 19:59:09 -0300

    manage error in product service

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,28 +1,37 @@
 import { ProductModel } from '../models/product.model';
 import { IProduct } from '../interfaces/product.interface';
 import { ObjectId } from 'mongoose';
+import logger from '../config/logger';
 
 export class ProductService {
-  async newProduct(product: IProduct): Promise<IProduct> {
-    const newProduct = new ProductModel(product);
-    return await newProduct.save();
+  async newProduct(product: IProduct): Promise<IProduct | undefined> {
+    try {
+      const newProduct = new ProductModel(product);
+      return await newProduct.save();
+    } catch (error) {
+      logger.error.error(error);
+    }
   }
 
   async updateProduct(
     id: ObjectId,
     product: IProduct,
   ): Promise<IProduct | null> {
-    return ProductModel.findOneAndUpdate(
-      { _id: id },
-      {
-        name: product.name,
-        price: product.price,
-        description: product.description,
-        category: product.category,
-        image: product.image,
-      },
-      { new: true },
-    );
+    try {
+      return ProductModel.findOneAndUpdate(
+        { _id: id },
+        {
+          name: product.name,
+          price: product.price,
+          description: product.description,
+          category: product.category,
+          image: product.image,
+        },
+        { new: true },
+      );
+    } catch (error) {
+      return null;
+    }
   }
 
   async getProducts(): Promise<IProduct[]> {
@@ -30,11 +39,19 @@ export class ProductService {
   }
 
   async getProduct(id: string): Promise<IProduct | null> {
-    return await ProductModel.findById(id);
+    try {
+      return await ProductModel.findById(id);
+    } catch (error) {
+      return null;
+    }
   }
 
   async deleteProduct(id: string): Promise<IProduct | null> {
-    return await ProductModel.findByIdAndDelete(id);
+    try {
+      return await ProductModel.findByIdAndDelete(id);
+    } catch (error) {
+      return null;
+    }
   }
 
   async getProductByCategory(category: string): Promise<IProduct[]> {
